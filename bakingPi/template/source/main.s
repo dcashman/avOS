d682d01c17dd985e4c331d9b890097dbd477ffb1
Daniel Alexander Cashman <dan.a.cashman@gmail.com>
2014-11-16 22:39:39 -0800

    OK05 solution

--- bakingPi/template/source/main.s
+++ bakingPi/template/source/main.s
@@ -18,25 +18,42 @@ main:
         .unreq pinFunc
 
         @begin timer
-        setVal .req r3
-        mov setVal, #0
+        ptrn .req r2
+        ldr ptrn, =sos_pattern
+        ldr ptrn, [ptrn]
+        seq .req r3
+        mov seq, #0
 delay_start:
-        push { setVal }
+        push { ptrn }
+        push { seq }
         waitTime .req r0
-        @ldr waitTime, =0x100000
-        ldr waitTime, =100000
+        ldr waitTime, =250000
         bl Wait
-        pop { setVal }
+        .unreq waitTime
+        pop { seq }
+        pop { ptrn }
 
-        @ switch setVal and set pin based on it
-        eor setVal, setVal, #1
+
         pinNum .req r0
         pinVal .req r1
+        @ get bit from sequence num and set pin 16 to that value
+        mov pinVal, #1
+        lsl pinVal, seq
+        ands pinVal, ptrn
+        push { ptrn }
+        push { seq }
+        movne pinVal, #1
         mov pinNum, #16
-        mov pinVal, setVal
-        push {setVal}
         bl SetGpio
-        pop {setVal}
+        pop { seq }
+        pop { ptrn }
         .unreq pinNum
         .unreq pinVal
+        add seq, #1
+        and seq, #0b11111
         b delay_start
+
+        .section .data
+        .align 2
+sos_pattern:
+        .int 0b11111111101010100010001000101010
